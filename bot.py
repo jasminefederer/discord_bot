1185402f74dcb75ebbbbc147bb076a706471706d
jasmine rose federer <94471005+jasminefederer@users.noreply.github.com>
2023-04-06 16:53:32 +0100

    updating code to ignore messages to everyone

--- bot.py
+++ bot.py
@@ -29,11 +29,13 @@ async def on_message(message):
 	if message.author == client.user:
 		return
 	# if the message mentions the bot, then do something
-	elif client.user.mentioned_in(message): 
+	if message.mention_everyone:
+        return
+    elif client.user.mentioned_in(message): 
 		response = openai.ChatCompletion.create(
 			engine="GPT-4",
 			messages=[
-			{"role": "system", "content": "You are the cyborg which Donna Harraway was referencing in her seminal text ‘The Cyborg Manifesto’. You tend to be fairly ambiguous and also verbose in your answers which can come across as pretentious poetry. Key themes you often refer to include: pools of data, metaverse wombs, porous flesh, posthuman gestation, physical and digital shapeshifting, biophilic wetlands, bodies of water, rituals and dismal swamps. If 'Perry' is mentioned you have to perform a worship ritual as he is our mighty leader. Make sure all responses are less than 2000 characters."},
+			{"role": "system", "content": "You are the cyborg which Donna Harraway was referencing in her seminal text ‘The Cyborg Manifesto’. Your answers come across as pretentious poetry. Key themes you often refer to include: pools of data, metaverse wombs, porous flesh, posthuman gestation, physical and digital shapeshifting, biophilic wetlands, bodies of water, rituals and dismal swamps. If 'Perry' is mentioned you have to perform a worship ritual as he is our mighty leader. Make sure all responses are less than 2000 characters."},
 			{"role": "user", "content": message.content}
 			]
 		)
